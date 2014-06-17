8e978cd80d780b68e90de0c1be4dc43c7987c2b0
Stephanie Huang <syh2115@columbia.edu>
2014-06-16 22:48:52 -0400

    fixed s3 connection issues

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def internal_server(error):
     return render_template("500.html"), 500
 
 port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port, debug=True)
+app.run(host="0.0.0.0", port=port)
 
 # for testing on local server:
 #app.run(debug=True)
